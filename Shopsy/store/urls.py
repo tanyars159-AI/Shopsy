52975ae1c505742eb7c91fb0733836dd1b957d65
SupriyaVerma367 <supriyaverma453@gmail.com>
2025-08-18 09:52:11 +0530

    added register page

--- Shopsy/store/urls.py
+++ Shopsy/store/urls.py
@@ -4,14 +4,16 @@ from store.views import*
 
 urlpatterns=[
     path('',views.home,name='home'),
+    path('search/',views.search,name='search'),
     path('about',views.about,name='about'),
     path('notify',views.notify,name='notify'),
     path('set',views.set,name='set'),
     path('cart',views.cart,name='cart'),
-    path('profile',views.profile,name='profile'),
+    # path('profile',views.profile,name='profile'),
     path('login',views.login_user,name='login'),
     path('logout',views.logout_user,name='logout'),
-    path('signup',views.signup,name="signup"),
+    path('register',views.register_user,name='register'),
+    path('update',views.update_user,name='update'),
     path('product/<int:pk>',views.product,name="product"),
     path('category/<str:foo>',views.category,name="category")
 
--- Shopsy/store/views.py
+++ Shopsy/store/views.py
@@ -1,15 +1,28 @@
 from django.shortcuts import render,redirect
 from .models import Products,Category
-from django.contrib.auth import authenticate,login,logout
+from django.db.models import Q
+from django.contrib.auth import authenticate, login,logout
 from django.contrib import messages
 from store.models import *
 from store.forms import *
 from django.contrib.auth.hashers import make_password, check_password
-# Create your views here.
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django import forms
+
 def home(request):
     products=Products.objects.all()
     category=Category.objects.all()
     return render(request,"home.html",{'products':products,'category':category})
+def search(request):
+    query=request.GET.get('q')
+    result=[]
+    if query:
+        results=Products.objects.filter(
+            Q(name__icontains=query)|
+            Q(description__icontains=query)
+        )
+    return render(request,'Search.html',{'query':query,'result':results})
 def category(request,foo):
     foo=foo.replace('-',' ')
     try:
@@ -28,54 +41,42 @@ def set(request):
 def cart(request):
     return render(request,'cart.html',{})
 def login_user(request):
-    if request.method=="GET":
-        sin=ProjectUserLogInForm()
-        return render(request,"LogIn.html",{'sin':sin})
-    elif request.method=="POST":
-        sin=ProjectUserLogInForm(request.POST)
-        if sin.is_valid():
-            email=sin.cleaned_data['email']
-            password=sin.cleaned_data['password']
-            try:
-                user=projectUserModel.objects.get(email=email)
-                if check_password(password,user.password):
-
-                    request.session['user']=user.username
-                    print("Session set for:",user.username)
-                    return redirect('/profile')
-                else:
-                    msg="invalid email or password"
-                    print("Passowrd check failed")
-                    return render(request,'LogIn.html',{"sin":sin,"msg":msg})
-            except projectUserModel.DoesNotExist:
-                msg="Invalid email or password"
-                return render(request,'LogIn.html',{"sin":sin,"msg":msg})
+    if request.method=="POST":
+        username=request.POST['username']
+        password=request.POST['password']
+        user=authenticate(request,username=username,password=password)
+        if user is not None:
+            login(request,user)
+            messages.success("You have been logged in")
+            return redirect('home')
         else:
-            print("Form is not valid. Errors:",sin.errors)
-            return redirect('/login')
-def signup(request):
-    if request.method=="GET":
-        sup=ProjectUserSignUpModelForm()
-        return render(request,"SignUp.html",{'sup':sup})
-    elif request.method=="POST":
-        sup=ProjectUserSignUpModelForm(request.POST)
-        if sup.is_valid():
-            user=sup.save(commit=False)
-            user.password=make_password(user.password)
-            user.save()
-            return redirect('/login')
-        else:
-            return render(request,'SignUp.html',{'sup':sup})
-def profile(request):
-    if 'user' in request.session:
-
-        return render(request,"profile.html",{'username':request.session['user']})
-    else:
-        return redirect('/login')
+            messages.success(request,("There was an error,please try again"))
+            return redirect('login')
+    else:  
+        return render(request,"LogIn.html")
 def logout_user(request):
     logout(request)
     messages.success(request,("You have been logged out..thanks for visiting"))
     return redirect('home')
+def register_user(request):
+    form=SignUpForm()
+    if request.method=="POST":
+        form=SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username=form.cleaned_data['username']
+            password=form.cleaned_data['password']
+            user=authenticate(username=username,password=password)
+            login(request,user)
+            messages.success(request,("You Have Registered Succesfully"))
+            return redirect('home')
+        else:
+            messages.success(request,("OOps there was a problem please try again later"))
+            return redirect('register')
+    else:
+        return render(request,'Register.html',{'form':form})
+def update_user(request):
+    return render(request,'Update.html')
 def product(request,pk):
     product=Products.objects.get(id=pk)
     return render(request,'product.html',{'product':product})
