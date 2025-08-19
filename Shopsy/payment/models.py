c3a5ec96b6cdfca3ec9f974b9b99bf1cf0b6b067
SupriyaVerma367 <supriyaverma453@gmail.com>
2025-08-19 22:14:47 +0530

    Checkout Payment

--- Shopsy/payment/models.py
+++ Shopsy/payment/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from store.models import Products
+from django.db.models.signals import post_save
 class ShippingAddress(models.Model):
     user=models.ForeignKey(User,on_delete=models.CASCADE,null=True,blank=True)
     shipping_full_name=models.CharField(max_length=255)
@@ -17,6 +18,11 @@ class ShippingAddress(models.Model):
         verbose_name_plural="Shipping Address"
     def __str__(self):
         return f'Shipping Address-{str(self.id)}'
+def create_shipping(sender,instance,created,**kwargs):
+    if created:
+        user_shipping=ShippingAddress(user=instance)
+        user_shipping.save()
+post_save.connect(create_shipping,sender=User)
 # Create order model
 class Order(models.Model):
     user=models.ForeignKey(User,on_delete=models.CASCADE,null=True,blank=True)
